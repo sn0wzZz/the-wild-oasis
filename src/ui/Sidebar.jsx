fcc1d921e8fa8665db65e163142cb1b1fbbf555a
sn0wzZz <m.stanchev1999@abv.bg>
2024-01-17 15:23:50 +0200

    Added conditional rendering to the Uploader

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -3,6 +3,7 @@ import Uploader from '../data/Uploader'
 
 import MainNav from './MainNav'
 import Logo from './Logo'
+import { useState } from 'react'
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
@@ -11,18 +12,25 @@ const StyledSidebar = styled.aside`
   grid-row: 1/-1;
   display: flex;
   flex-direction: column;
-   gap: 3.2rem;
-   box-shadow: var(--shadow-lg);
-   /* border-radius: 0 3rem 3rem 0; */
-   z-index: 99;
+  gap: 3.2rem;
+  box-shadow: var(--shadow-lg);
+  /* border-radius: 0 3rem 3rem 0; */
+  z-index: 99;
 `
 
 export default function Sidebar() {
+  const [showUploader, setShowUploader] = useState(false)
+
   return (
     <StyledSidebar>
       <Logo />
       <MainNav />
-      <Uploader />
+      <div
+        onDoubleClick={() => setShowUploader((cur) => !cur)}
+        style={{ visibility: showUploader ? 0 : 1 }}
+      >
+        <Uploader />
+      </div>
     </StyledSidebar>
   )
 }
